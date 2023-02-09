a5ffba44dd7ca90bbb6433712279b4be615131e6
Andres <adcr2106@gmail.com>
2023-02-08 23:57:44 -0500

    Implement exercise 5

---
+++ ejercicio/ejercicio_5.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    def fahrenheit_a_celsius(fahrenheit):
+        celsius = (fahrenheit - 32) / 1.8
+        print(fahrenheit,"grados fahrenheit es a celsius", celsius,"°")
+
+
+    fahrenheit_a_celsius(200)
