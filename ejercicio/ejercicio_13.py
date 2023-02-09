949fab79f31ec7207557dbd0ae44f5f7f2f8f34e
Andres <adcr2106@gmail.com>
2023-02-09 10:18:36 -0500

    Implement exercise 13

---
+++ ejercicio/ejercicio_13.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    numero1 = int(input("Digite su primer numero: "))
+    numero2 = int(input("Digite su segundo numero: "))
+    print("La suma de los dos numeros es: ", (numero1 + numero2))
+    print("La resta de los dos numeros es: ", (numero1 - numero2))
+    print("La multiplicación de los dos numeros es: ", (numero1 * numero2))
+    print("La división de los dos numeros es: ", (numero1 / numero2))
