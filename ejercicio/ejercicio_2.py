050c19e8b90a917fd4594e57ebb4f82cdbf383ec
Andres <adcr2106@gmail.com>
2023-02-08 23:42:31 -0500

    Implement exercise 2

---
+++ ejercicio/ejercicio_2.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    def area_circulo_radio(radio):
+        area_circulo = (radio * radio) * 3.14
+        print("El area del circulo es: ", area_circulo)
+
+area_circulo_radio(5)
